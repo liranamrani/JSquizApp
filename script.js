b7e3588c40fdbaa684e254f520c5707e7bffa71a
Liran Amrani <amrani2007@gmail.com>
2020-10-07 23:51:10 +0300

    FinishProject

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 const quizData = [
     {
     quistion : "When Beitar Jerusalem foundate?",
-    A : '1938',
+    A : '1936',
     B : '1935',
-    C : '1948',
+    C : '1938',
     D : '1928',
     correct: 'A'
     },
     {
-    quistion : "What is the most popular language in 2019?",
-    A : 'JAVA',
-    B : 'Python',
-    C : 'JS',
-    D : 'C#',
-    correct: 'A'
+    quistion : "In which year did Maradona score a goal with his hand?",
+    A : '1990',
+    B : '1986',
+    C : '1982',
+    D : '1978',
+    correct: 'B'
     },
     {
-    quistion : "Who is the primeminister of Israel?",
-    A : 'Trump',
-    B : 'Benjamin Netanyahu',
-    C : 'Gants',
-    D : 'Sara Netanayhu',
+    quistion : "How many minutes is a rugby match?",
+    A : '90',
+    B : '70',
+    C : '60',
+    D : '80',
     correct: 'D'
     }
     
@@ -28,6 +28,8 @@ const quizData = [
 
 
 let currentQuestion = 0 ;
+let trueAnswers = 0;
+const quizWindow = document.getElementById('quiz-header');
 const Quiz_question = document.getElementById('question_text');
 const A_ans = document.getElementById('a_text');
 const B_ans = document.getElementById('b_text');
@@ -36,6 +38,7 @@ const D_ans = document.getElementById('d_text');
 const chosenRadioBtn = document.getElementById('answer');
 const subnitBtn = document.getElementById('submitBtn');
 loadQuiz();
+
 
 function loadQuiz(){
     const question = quizData[currentQuestion].quistion;
@@ -56,15 +59,51 @@ function loadQuiz(){
 }
 
 subnitBtn.addEventListener("click", () => {
+    getSelectedBtn();
     currentQuestion++;
     if (currentQuestion < quizData.length)
     {
     loadQuiz();
+    deselectAnswer();
     }
     else
     {
-        //TODO: Show result.
-        alert('You finish all the Quiz!!')
+        quizWindow.innerHTML = `<button id="tryAgainBtn" onclick=location.reload()>Try Again</button>
+        <h3>You had finished the Quiz - Final result: ${trueAnswers}/${currentQuestion}</h3>`;
+        subnitBtn.disabled = true;
+        
     }
 
 })
+
+function deselectAnswer(){
+    const answers = document.getElementsByName('answer');
+    answers.forEach(answer => {
+        answer.checked = false;
+    });
+}
+
+function getSelectedBtn(){
+    let flag = false;
+    const answers = document.getElementsByName('answer');
+    answers.forEach(answer => {
+        if (answer.checked == true)
+        {
+            flag = true;
+            if (answer.id == quizData[currentQuestion].correct)
+            {
+            trueAnswers++;
+            }
+            else
+            {
+            console.log("Bad");
+            }
+        }
+        
+    });
+    if (!flag)
+    {
+        alert("You Have To Answer..")
+        currentQuestion--;
+    }
+}
